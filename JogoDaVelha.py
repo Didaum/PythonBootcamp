52268cbff0a18ca59228de706d7891b88d986d30
Rodrigo Zanini <rhpzanini@gmail.com>
2018-05-17 17:11:57 -0300

    Substituido For na Jogada

--- JogoDaVelha.py
+++ JogoDaVelha.py
@@ -4,9 +4,9 @@ jogador2 = input('Digite o nome do Jogador 2: ')
 # Exemplo das posições
 print('\n')
 print('POSIÇÔES')
-print('{} | {} | {}'.format('7', '8', '9'))
-print('{} | {} | {}'.format('4', '5', '6'))
-print('{} | {} | {}'.format('1', '2', '3'))
+print('7 | 8 | 9')
+print('4 | 5 | 6')
+print('1 | 2 | 3')
 
 # Matriz de posições
 posicao_lista1 = [0] * 3
@@ -22,29 +22,31 @@ simbolo_total = [simbolo_lista1, simbolo_lista2, simbolo_lista3]
 
 
 # Gera o tabuleiro com a jogada
-def gera_jogada():
-    for i in range(0, 3):
-        for m in range(0, 3):
-            if posicao_total[i][m] == 1:
-                simbolo_total[i][m] = 'X'
-            elif posicao_total[i][m] == -1:
-                simbolo_total[i][m] = 'O'
+def board():
+    for linha in range(0, 3):
+        for coluna in range(0, 3):
+            if posicao_total[linha][coluna] == 1:
+                simbolo_total[linha][coluna] = 'X'
+            elif posicao_total[linha][coluna] == -1:
+                simbolo_total[linha][coluna] = 'O'
     print('{} | {} | {}'.format(simbolo_total[2][0], simbolo_total[2][1], simbolo_total[2][2]))
     print('{} | {} | {}'.format(simbolo_total[1][0], simbolo_total[1][1], simbolo_total[1][2]))
     print('{} | {} | {}'.format(simbolo_total[0][0], simbolo_total[0][1], simbolo_total[0][2]))
 
 
-def vencedor():
+# Percorre a matriz e soma as coluna, linhas ou diagonais
+def vitoria():
     soma1 = 0
     soma2 = 0
-    for y in range(0, 3):
-        for n in range(0, 3):
-            soma1 = soma1 + posicao_total[y][n]
-            soma2 = soma2 + posicao_total[n][y]
-        if soma1 == 3 or soma2 == 3:
-            print('{} é o Vencedor'.format(jogador1))
-        elif soma1 == -3 or soma2 == -3:
-            print('{} é o Vencedor'.format(jogador2))
+    for linha in range(0, 3):
+        for coluna in range(0, 3):
+            soma1 = soma1 + posicao_total[linha][coluna]
+            soma2 = soma2 + posicao_total[coluna][linha]
+    if soma1 == 3 or soma2 == 3:
+        print('{} é o Vencedor'.format(jogador1))
+    elif soma1 == -3 or soma2 - 3:
+        print('{} é o Vencedor'.format(jogador2))
+
     diagonal1 = posicao_total[0][0] + posicao_total[1][1] + posicao_total[2][2]
     diagonal2 = posicao_total[0][2] + posicao_total[1][1] + posicao_total[2][0]
     if diagonal1 == 3 or diagonal2 == 3:
@@ -53,31 +55,32 @@ def vencedor():
         print('{} é o Vencedor'.format(jogador2))
 
 
-for jogada in range(1, 10):
-    if (jogada % 2) != 0:
-        posicao = int(input(f'\nDigite uma posição {jogador1}: '))
-        if posicao == 1:
-            posicao_total[0][0] = 1
-        elif posicao == 2:
-            posicao_total[0][1] = 1
-        elif posicao == 3:
-            posicao_total[0][2] = 1
-        elif posicao == 4:
-            posicao_total[1][0] = 1
-        elif posicao == 5:
-            posicao_total[1][1] = 1
-        elif posicao == 6:
-            posicao_total[1][2] = 1
-        elif posicao == 7:
-            posicao_total[2][0] = 1
-        elif posicao == 8:
-            posicao_total[2][1] = 1
-        elif posicao == 9:
-            posicao_total[2][2] = 1
-        gera_jogada()
-        vencedor()
+jogador = 1
 
-    else:
+while jogador == 1:
+    posicao = int(input(f'\nDigite uma posição {jogador1}: '))
+    if posicao == 1:
+        posicao_total[0][0] = 1
+    elif posicao == 2:
+        posicao_total[0][1] = 1
+    elif posicao == 3:
+        posicao_total[0][2] = 1
+    elif posicao == 4:
+        posicao_total[1][0] = 1
+    elif posicao == 5:
+        posicao_total[1][1] = 1
+    elif posicao == 6:
+        posicao_total[1][2] = 1
+    elif posicao == 7:
+        posicao_total[2][0] = 1
+    elif posicao == 8:
+        posicao_total[2][1] = 1
+    elif posicao == 9:
+        posicao_total[2][2] = 1
+    board()
+    vitoria()
+    jogador = -1
+    while jogador == -1:
         posicao = int(input(f'\nDigite uma posição {jogador2}: '))
         if posicao == 1:
             posicao_total[0][0] = -1
@@ -97,5 +100,6 @@ for jogada in range(1, 10):
             posicao_total[2][1] = -1
         elif posicao == 9:
             posicao_total[2][2] = -1
-        gera_jogada()
-        vencedor()
+        board()
+        vitoria()
+        jogador = 1
