9b522423e7cab9464a635d4e656d8242e8bea554
Rodrigo Zanini <rhpzanini@gmail.com>
2018-05-24 18:45:18 -0300

    Atualizado função Vitoria

--- JogoDaVelha.py
+++ JogoDaVelha.py
@@ -34,28 +34,66 @@ def board():
     print('{} | {} | {}'.format(simbolo_total[0][0], simbolo_total[0][1], simbolo_total[0][2]))
 
 
+jogador = 1
+
+
 # Percorre a matriz e soma as coluna, linhas ou diagonais
 def vitoria():
-    soma1 = 0
-    soma2 = 0
-    for linha in range(0, 3):
-        for coluna in range(0, 3):
-            soma1 = soma1 + posicao_total[linha][coluna]
-            soma2 = soma2 + posicao_total[coluna][linha]
-    if soma1 == 3 or soma2 == 3:
+    # Identifica se algum jogador completou alguma linha
+    somal1 = 0
+    somal2 = 0
+    somal3 = 0
+    for linha1 in posicao_lista1:
+        somal1 += linha1
+        if somal1 == 3:
+            print('{} é o Vencedor'.format(jogador1))
+        elif somal1 == -3:
+            print('{} é o Vencedor'.format(jogador2))
+    for linha2 in posicao_lista2:
+        somal2 += linha2
+        if somal2 == 3:
+            print('{} é o Vencedor'.format(jogador1))
+        elif somal2 == -3:
+            print('{} é o Vencedor'.format(jogador2))
+    for linha3 in posicao_lista3:
+        somal3 += linha3
+        if somal3 == 3:
+            print('{} é o Vencedor'.format(jogador1))
+        elif somal3 == -3:
+            print('{} é o Vencedor'.format(jogador2))
+    # Identifica se algum jogador completou alguma coluna
+    somac1 = 0
+    somac2 = 0
+    somac3 = 0
+    for coluna1 in range(0, 3):
+        col1 = 0
+        somac1 += posicao_total[coluna1][col1]
+        if somac1 == 3:
+            print('{} é o Vencedor'.format(jogador1))
+        elif somac1 == -3:
+            print('{} é o Vencedor'.format(jogador2))
+    for coluna2 in range(0, 3):
+        col2 = 1
+        somac2 += posicao_total[coluna2][col2]
+        if somac2 == 3:
+            print('{} é o Vencedor'.format(jogador1))
+        elif somac2 == -3:
+            print('{} é o Vencedor'.format(jogador2))
+    for coluna3 in range(0, 3):
+        col3 = 2
+        somac3 += posicao_total[coluna3][col3]
+        if somac3 == 3:
+            print('{} é o Vencedor'.format(jogador1))
+        elif somac3 == -3:
+            print('{} é o Vencedor'.format(jogador2))
+    # Identifica se algum jogador completou alguma diagonal
+    somad1 = posicao_total[0][0] + posicao_total[1][1] + posicao_total[2][2]
+    somad2 = posicao_total[2][0] + posicao_total[1][1] + posicao_total[0][2]
+    if somad1 == 3 or somad2 == 3:
         print('{} é o Vencedor'.format(jogador1))
-    elif soma1 == -3 or soma2 - 3:
+    elif somad1 == -3 or somad2 == -3:
         print('{} é o Vencedor'.format(jogador2))
 
-    diagonal1 = posicao_total[0][0] + posicao_total[1][1] + posicao_total[2][2]
-    diagonal2 = posicao_total[0][2] + posicao_total[1][1] + posicao_total[2][0]
-    if diagonal1 == 3 or diagonal2 == 3:
-        print('{} é o Vencedor'.format(jogador1))
-    elif diagonal1 == -3 or diagonal2 == -3:
-        print('{} é o Vencedor'.format(jogador2))
-
-
-jogador = 1
 
 while jogador == 1:
     posicao = int(input(f'\nDigite uma posição {jogador1}: '))
@@ -78,8 +116,8 @@ while jogador == 1:
     elif posicao == 9:
         posicao_total[2][2] = 1
     board()
-    vitoria()
     jogador = -1
+    vitoria()
     while jogador == -1:
         posicao = int(input(f'\nDigite uma posição {jogador2}: '))
         if posicao == 1:
@@ -101,5 +139,5 @@ while jogador == 1:
         elif posicao == 9:
             posicao_total[2][2] = -1
         board()
-        vitoria()
         jogador = 1
+        vitoria()
